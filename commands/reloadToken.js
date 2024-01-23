83ea12b8e72beb06b5ff96c13a4e64a9203a0142
neomoth <69372252+neomoth@users.noreply.github.com>
2024-01-23 00:49:17 -0500

    Add user feedback for regenerating token

--- commands/reloadToken.js
+++ commands/reloadToken.js
@@ -7,5 +7,6 @@ module.exports = {
 		if(e.senderUsername!=='neomothdev') return;
 		client.refresh();
 		console.log(`[INFO]: regenerated token at ${new Date().toLocaleString()}`);
+		client.me(e.channelName, `Successfully regenerated client token.`);
 	}
 }
