23f877704d069400c8483b189d0a25a46a9a7d77
neomoth <69372252+neomoth@users.noreply.github.com>
2024-01-23 00:33:00 -0500

    make so only bot dev can regen token

--- commands/reloadToken.js
+++ commands/reloadToken.js
@@ -4,6 +4,7 @@ module.exports = {
 		description:'Force a refresh of the bot token',
 		aliases:['rt']
 	},async run(client,e){
+		if(e.senderUsername!=='neomothdev') return;
 		client.refresh();
 	}
 }
