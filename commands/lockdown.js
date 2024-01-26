8299e30b742bf90a409a3c3d9a745d5a1c9acf16
neomoth <69372252+neomoth@users.noreply.github.com>
2024-01-26 17:52:26 -0500

    update lockdown

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -8,14 +8,17 @@ module.exports = {
 		if(e.senderUsername!=='neomothdev')return;
 		if(args[1]===null||args[1]===' '||(args[1]!=='false'&&args[1]!=='true')){
 			global.lockdown=!global.lockdown;
+			client.privmsg(e.channelName, `/me Bot lockdown status toggled to ${global.lockdown}`);
 			return;
 		}
 		if(args[1]==='false'){
 			global.lockdown=false;
+			client.privmsg(e.channelName, `/me Bot is no longer on lockdown`);
 			return;
 		}
 		if(args[1]==='true'){
 			global.lockdown=true;
+			client.privmsg(e.channelName,`/me Bot is now on lockdown!`);
 		}
 	}
 }
